f1789a9c4ecc69d00df5f921bc21bf0229392223
tgardela <king_nothin@o2.pl>
2017-06-22 12:55:10 +0200

    rest of chapter 4 code

--- C04/scanfile.py
+++ C04/scanfile.py
@@ -1,8 +1,14 @@
 def scanner(name, function):
-    file = open(name, 'r')
-    while True:
-        line = file.readline()
-        if not line:
-            break
+    for line in open(name, 'r'):
         function(line)
-    file.close()
+
+
+#minimalistic versions:
+# def scanner(name, function):
+#     list(map(function, open(name, 'r')))
+#
+# def scanner(name, function):
+#     [function(line) for line in open(name, 'r')]
+#
+# def scanner(name, function):
+#     list(function(line) for line in open(name, 'r'))
